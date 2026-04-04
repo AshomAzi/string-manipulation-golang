d4a7fa70c93053b0253a21e1afd94d5f7edb5452
Ashom Azi <ashomazibulus@gmail.com>
2026-04-04 22:39:05 +0100

    Case Swapper

--- 5_case_swapper/main.go
+++ 5_case_swapper/main.go
@@ -10,19 +10,28 @@ import (
 )
 
 func main() {
-	fmt.Print("Input your text: ")
+start1:
+	fmt.Print("Input your text or exit to cancel: ")
 	text := bufio.NewScanner(os.Stdin)
 
 	for text.Scan() {
 		line := text.Text()
-		var new string
-		for i := 0; i < len(line); i++ {
-			if line[i] >= 'a' && line[i] <= 'z' {
-				new += strings.ToUpper(string(line[i]))
-			} else if line[i] >= 'A' && line[i] <= 'Z' {
-				new += strings.ToLower(string(line[i]))
+		var new strings.Builder
+		if line == "exit" || line == "EXIT" {
+			break
+		} else if len(line) > 0 {
+			for i := 0; i < len(line); i++ {
+				if line[i] >= 'a' && line[i] <= 'z' {
+					new.WriteString(strings.ToUpper(string(line[i])))
+				} else if line[i] >= 'A' && line[i] <= 'Z' {
+					new.WriteString(strings.ToLower(string(line[i])))
+				}
 			}
+			fmt.Println(new.String())
+		} else {
+			fmt.Println("Invalid Input, try again!")
+			goto start1
 		}
-		fmt.Println(new)
+
 	}
 }
